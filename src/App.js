da0cee4b0d0a0417aad72e1252a179273acab51c
Chris Carr <christocarr@outlook.com>
2019-12-31 11:52:54 +0000

    Add image link form

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Nav from './components/Nav';
 import Logo from './components/logo/Logo';
+import ImageLink from './components/image-link/ImageLink'
 import './App.css';
 
 function App() {
@@ -8,8 +9,8 @@ function App() {
     <div className="App">
       <Nav />
       <Logo />
-      {/*<ImageLink />
-      <FaceRegocnition /> */}
+      <ImageLink />
+      {/*<FaceRegocnition /> */}
     </div>
   );
 }
---
+++ src/components/image-link/ImageLink.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ImageLink = () => {
+  return (
+    <form className="flex">
+      <input type="text" />
+      <button>Detect</button>
+    </form>
+  )
+}
+
+export default ImageLink

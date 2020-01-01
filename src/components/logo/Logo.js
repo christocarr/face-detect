1a8ca470a32d6c07f20c736b383eda2cf78d506a
Chris Carr <christocarr@outlook.com>
2020-01-01 17:55:12 +0000

    Change logo size

--- src/components/logo/Logo.js
+++ src/components/logo/Logo.js
@@ -5,7 +5,7 @@ import icon from './icon.svg'
 const Logo = () => {
   return (
     <div>
-      <Tilt className="Tilt" options={{max: 25}} style={{ height: '10rem', width: '10rem' }}>
+      <Tilt className="Tilt" options={{max: 25}} style={{ height: '5rem', width: '5rem' }}>
         <div className="Tilt-inner bg-near-white shadow-5" style={{ height: '100%', width: '100%' }}>
           <img src={icon} alt="icon"/>
         </div>

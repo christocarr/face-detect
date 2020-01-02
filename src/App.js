53504296dd85d668700139c97e57ecaaf35f8098
Chris Carr <christocarr@outlook.com>
2020-01-02 19:39:17 +0000

    Change to class component and add state for input

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Header from './components/Header'
 import ImageLink from './components/image-link/ImageLink'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <Header />
-      <ImageLink />
-      {/*<FaceRegocnition /> */}
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      input: ''
+    }
+  }
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <ImageLink />
+        {/*<FaceRegocnition /> */}
+      </div>
+    );
+  }
 }
 
 export default App;

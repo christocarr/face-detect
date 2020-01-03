cfc76a306d0f8df9dfd6d7c65cdbb2e7232963d4
Chris Carr <christocarr@outlook.com>
2020-01-03 18:18:35 +0000

    Get response face detection bounding box

--- src/App.js
+++ src/App.js
@@ -32,7 +32,7 @@ class App extends Component {
       )
       .then(
         function(response) {
-          console.log(response);
+          console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
         },
         function(err) {}
       );

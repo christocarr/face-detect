e9a2a7ff49093b43a7ceed8517d0f2f197f6a035
Chris Carr <christocarr@outlook.com>
2020-01-04 09:57:27 +0000

    Add css for bounding box

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
 import Header from './components/Header';
 import ImageLink from './components/image-link/ImageLink';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import './App.css';
 import Clarifai from 'clarifai';
-import FaceRecognition from './components/FaceRecognition';
 
 const app = new Clarifai.App({
   apiKey: 'e263d09d6e604310b00efcae3f385aa8',
--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import './FaceRecognition.css'
 
-const FaceRecognition = ({ imgUrl }) => {
-  console.log(imgUrl)
+const FaceRecognition = ({ box, imgUrl }) => {
+  console.log(box)
   return (
     <div className="mt3">
       <img id="image" src={imgUrl} alt="" width="300px" height="auto" />
+      <div className="bounding-box"></div>
     </div>
   );
 };
